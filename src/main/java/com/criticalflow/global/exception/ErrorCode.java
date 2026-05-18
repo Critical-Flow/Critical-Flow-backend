7f4d9fcf075d8296356ef26d748c7bf7dd15a489
silverblond <kdg23538@yu.ac.kr>
2026-05-18 20:18:23 +0900

    feat: USER_NOT_FOUND ErrorCode 추가

--- src/main/java/com/criticalflow/global/exception/ErrorCode.java
+++ src/main/java/com/criticalflow/global/exception/ErrorCode.java
@@ -22,7 +22,10 @@ public enum ErrorCode {
 
     // Auth
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "INVALID_REFRESH_TOKEN", "유효하지 않은 RefreshToken입니다."),
-    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "EXPIRED_REFRESH_TOKEN", "만료된 RefreshToken입니다.");
+    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "EXPIRED_REFRESH_TOKEN", "만료된 RefreshToken입니다."),
+
+    // User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
