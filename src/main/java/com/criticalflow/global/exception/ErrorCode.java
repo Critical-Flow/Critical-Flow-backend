5241eb2f3263d2de1f97fee098fc37013db569ee
silverblond <kdg23538@yu.ac.kr>
2026-05-19 18:00:25 +0900

    feat: CATEGORY_NOT_FOUND ErrorCode 추가

--- src/main/java/com/criticalflow/global/exception/ErrorCode.java
+++ src/main/java/com/criticalflow/global/exception/ErrorCode.java
@@ -25,7 +25,10 @@ public enum ErrorCode {
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "EXPIRED_REFRESH_TOKEN", "만료된 RefreshToken입니다."),
 
     // User
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다."),
+
+    // Category
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY_NOT_FOUND", "카테고리를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
